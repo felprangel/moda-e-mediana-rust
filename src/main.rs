566d70c58e9af259cccec8267fc681b9d7d25fde
felprangel <feliperangel897@gmail.com>
2024-11-13 19:55:09 -0300

    adiciona o input do usuário

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Digite o primeiro número: ");
+
+    let mut num1 = String::new();
+
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Falha ao ler a linha");
+
+    println!("Digite o segundo número: ");
+
+    let mut num2 = String::new();
+
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Falha ao ler a linha");
+
+    println!("Digite o terceiro número: ");
+
+    let mut num3 = String::new();
+
+    io::stdin()
+        .read_line(&mut num3)
+        .expect("Falha ao ler a linha");
+
+    println!("Digite o quarto número: ");
+
+    let mut num4 = String::new();
+
+    io::stdin()
+        .read_line(&mut num4)
+        .expect("Falha ao ler a linha");
+
+    println!("Digite o quinto número: ");
+
+    let mut num5 = String::new();
+
+    io::stdin()
+        .read_line(&mut num5)
+        .expect("Falha ao ler a linha");
 }
